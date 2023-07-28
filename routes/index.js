5d6560c3faf8b9f54c0f3d4956c7b05ca90550a9
xuanhoang <102515365+NXHoang1212@users.noreply.github.com>
2023-07-28 16:51:32 +0700

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//https:localhost:3000/index
+router.get('/index', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 
 
 module.exports = router;
